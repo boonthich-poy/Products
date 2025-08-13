b5b277d5cd8ce830f09cdce1ab7bf97db35d34ff
boonthich-poy <boonthich-poy@example.com>
2025-08-13 21:27:24 +0700

    add Product and Store

--- Product and Store.py
+++ Product and Store.py
@@ -1,21 +1,25 @@
 class Product:
     def __init__(self, name, quantity):
-        self.name = name
-        self.quantity = quantity
+        self.name = str(name)
+        self.quantity = int(quantity)
 
 class Store:
     def __init__(self):
-        self.products = []
+        self.__products = {}
 
     def add_product(self, name, quantity):
-        product = Product(name, quantity)
-        self.products.append(product)
+        obj = Product(name, quantity)
+        if obj.name in self.__products:
+            self.__products[obj.name].quantity += obj.quantity
+        else:
+            self.__products[obj.name] = obj
 
     def show_products(self):
-        for p in self.products:
-            print(f"สินค้า: {p.name}, จำนวน: {p.quantity}")
+        for name ,obj in self.__products.items():
+            print(f"{obj.name}  :  {obj.quantity}")
 
 my_store = Store()
 my_store.add_product("โฟมล้างหน้า", 30)
 my_store.add_product("ทิชชู่", 15)
+my_store.add_product("โฟมล้างหน้า", 60) 
 my_store.show_products()
